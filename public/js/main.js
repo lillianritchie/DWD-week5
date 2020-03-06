581347a0ae8a264407e36c78449b85df6f6ba304
lillianritchie <lr2527@nyu.edu>
2020-03-06 16:28:53 -0500

    whoops

--- public/js/main.js
+++ public/js/main.js
@@ -22,10 +22,20 @@ function listTodos(data) {
     const myListElements =[]
     for(let i =0; i <data.length; i++ ){
         let item = data[i];
+        let status;
+        if(item.status == "not started"){
+            status = "a";
+        } else if( item.status == "in progress"){
+            status = "b";
+        } else if (item.status == "complete"){
+            status = "c";
+        } else {
+            status = "d";
+        }
         myListElements.push(`<li data-todo="${item.todo}">
-                                <span class="todo-status">${item.status}</span>
+                                <span class="status__${status}"> (${status}) </span>
                                 <span>${item.todo}</span>
-                                <button onclick="removeTodo"> delete it </button>
+                                <button class="todo__delete"> delete it </button>
                             </li>`);
         
         //todoItems.append(todoItem);
@@ -34,3 +44,19 @@ function listTodos(data) {
     
     //console.log(HELP);
 }
+const $deleteButtons = document.querySelectorAll(".todo__delete");
+
+$deleteButtons.forEach( item => {
+    item.addEventListener("click", async(evt) => {
+        console.log("trying to delete");
+        try{
+            const id = evt.target._id;
+            await fetch (`/api/v1/todos/${id}`, {method: "DELETE"});
+            console.log("delete successful");
+        } catch(error){
+            console.log("OH NO! Somethign is wrong");
+        }
+    }
+    )
+    
+});
